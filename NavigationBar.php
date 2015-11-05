e6f062a5d039a81bcebdaa365f4da9ed17bd2356
Rafael Marques <rcmarques@me.com>
2015-11-05 06:53:07 -0200

    Forgot to put this in the last one

---
+++ NavigationBar.php
@@ -0,0 +1,21 @@
+<ul class = "logo">
+            <li><img src = "Student%20Vanguard%20Logo%20BIG.png"></li>
+        </ul>
+     <div class="nav">
+          <div class="container">
+            <ul class="pull-left">
+                <li><a href="Main.html">Home</a></li>
+                <li><a href="Dashboard.html">Dashboard</a></li>
+                <li><a href="News.html">News</a></li>
+                <li><a href="Student_Council.html">Student Council</a></li>
+                <li><a href="Iris.html">IRIS</a></li>
+                <li><a href = "Digital_Senate.html">Digital Senate</a></li>
+            </ul>
+            <ul class="pull-right">
+              <li><a href="Contact_Us.html">Contacts</a></li>
+              <li><a href="About_Us.html">About Us</a></li>
+              <li><a href="Donate.html">Donate</a></li>
+                <li><a href = "Log%20In.html">Log In</a></li>
+            </ul>
+          </div>
+        </div>

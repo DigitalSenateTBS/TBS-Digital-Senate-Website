ff9d64d4a3f0dceec11d0a6d0c33ebf1a5fbff55
PotatoProgrammer <anttonipykalisto@gmail.com>
2016-02-08 00:47:23 -0200

    Connecting to MongoDB
    Mongo, we have a problem...

--- Main.php
+++ Main.php
@@ -1,4 +1,9 @@
 <!DOCTYPE HTML>
+<?php
+$connection = new MongoClient(); // connects to localhost:27017
+$connection = new MongoClient( "mongodb://example.com" ); // connect to a remote host (default port: 27017)
+$connection = new MongoClient( "mongodb://example.com:65432" ); // connect to a remote host at a given port
+?>
 <html>
     <head>
         <link rel="shortcut icon" href="StudentVanguardLogo.ico">
